be2fff266e2f8db7454ba29274d9c0452ae875ec
alimills <ali@jambool.com>
2011-06-01 16:06:01 -0700

    stubbing some create and read rpc methods on observations controller

--- api/controllers/observations.py
+++ api/controllers/observations.py
@@ -1,17 +1,46 @@
+import datetime
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 
+from protorpc import message_types
 from protorpc import messages
 from protorpc import remote
 
-class Request(messages.Message):
-  my_name = messages.StringField(1, required=True)
+class ObservationRequest(messages.Message):
+ 	id = messages.StringField(1)
+	lat = messages.IntegerField(2)
+	lng = messages.IntegerField(3)
+	limit = messages.IntegerField(4, default=10)
 
-class Response(messages.Message):
-  hello = messages.StringField(1, required=True)
+class ObservationResponse(messages.Message):
+#	observed_at = messages.IntegerField(1)
+	lat = messages.IntegerField(2)
+	lng = messages.IntegerField(3)
+
+class ObservationsResponse(messages.Message):
+  observations = messages.MessageField(ObservationRequest, 1, repeated=True)
 
 class Service(remote.Service):
 
-  @remote.method(Request, Response)
-  def hello(self, request):
-    return Response(hello='Hello there, %s!' % request.my_name)
+  @remote.method(ObservationRequest, message_types.VoidMessage)
+  def create(self, request):
+		return message_types.VoidMessage()
+
+  @remote.method(ObservationRequest, ObservationResponse)
+  def by_id(self, request):
+    return self.create_observation()
+
+  @remote.method(ObservationRequest, ObservationsResponse)
+  def by_lat_lng(self, request):
+    return self.create_observation()
+
+  @remote.method(ObservationRequest, ObservationsResponse)
+  def all(self, request):
+		return ObservationsResponse(observation=[self.create_observation(),self.create_observation()])
+
+  def create_observation(self):
+		observed_at = datetime.datetime.now()
+		lat = 123
+		lng = 456
+		return ObservationResponse(lat=lat,lng=lng)
