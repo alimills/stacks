6c37725a5be384ba9b9e4c6170343f9306112a45
Luke Bayes <lbayes@google.com>
2011-06-01 13:37:02 -0700

    Moved python stuff to python lib

--- test/test_observation.py
+++ test/test_observation.py
@@ -2,15 +2,34 @@
 #
 # Copyright 2011 Google Inc. All Rights Reserved.
 
-"""Tests for observation."""
+"""Tests for observation.
+
+Found example here:
+http://code.google.com/appengine/docs/python/tools/localunittesting.html
+
+"""
 
 __author__ = 'lbayes@google.com (Luke Bayes)'
 
 #from google3.testing.pybase import googletest
 import unittest
-from api.models import observation
+import datetime
+from google.appengine.ext import db
+from google.appengine.ext import testbed
+from api.models.observation import Observation
 
 class TestObservation(unittest.TestCase):
 
-  def testGiveMeAName(self):
-    self.fail("BROKE")
+  def setup(self):
+    self.testbed = testbed.Testbed()
+    self.testbed.activate()
+    self.testbed.init_datastore_v3_stub()
+
+  def teardown(self):
+    self.testbed.deactivate()
+
+  def testCreate(self):
+    observation = Observation()
+    observation.station_id = 'abcd'
+    observation.observed_at = datetime.datetime.now()
+    observation.put()
