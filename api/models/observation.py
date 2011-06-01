d0683d4f35d2abe6b733caaa0879baee29fc5e09
Luke Bayes <lbayes@google.com>
2011-06-01 10:47:56 -0700

    Working on model

--- api/models/observation.py
+++ api/models/observation.py
@@ -12,10 +12,9 @@ http://www.ndbc.noaa.gov/data/latest_obs/latest_obs.txt
 __author__ = 'lbayes@google.com (Luke Bayes)'
 
 from google.appengine.ext import db
+from api.lib.geomodel import GeoModel
 
-class Observation(db.Model):
-  lat = db.StringProperty()
-  lng = db.StringProperty()
+class Observation(GeoModel)
   station_id = db.StringProperty()
   created_at = db.DateTimeProperty(auto_now_add = True)
   observed_at = db.DateTimeProperty()
