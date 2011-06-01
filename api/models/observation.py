efbd136ccfa92e54539e72bb0f1b5645e38948b4
Luke Bayes <lbayes@google.com>
2011-06-01 16:49:13 -0700

    Added station, removing some comments

--- api/models/observation.py
+++ api/models/observation.py
@@ -1,16 +1,3 @@
-#!/usr/bin/python2.4
-#
-# Copyright 2011 Google Inc. All Rights Reserved.
-
-"""The base class for all geospatial, natural observations.
-
-Some example observations can be found here:
-
-http://www.ndbc.noaa.gov/data/latest_obs/latest_obs.txt
-"""
-
-__author__ = 'lbayes@google.com (Luke Bayes)'
-
 from google.appengine.ext import db
 from geo.geomodel import GeoModel
 
--- test/test_observation.py
+++ test/test_observation.py
@@ -1,16 +1,3 @@
-#!/usr/bin/python2.4
-#
-# Copyright 2011 Google Inc. All Rights Reserved.
-
-"""Tests for observation.
-
-Found example here:
-http://code.google.com/appengine/docs/python/tools/localunittesting.html
-
-"""
-
-__author__ = 'lbayes@google.com (Luke Bayes)'
-
 import datetime
 from test.stack_test_case import StackTestCase
 from api.models.observation import Observation
