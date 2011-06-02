67c2073742cf2d0e4bb813fd90a0a48e43cb2c5c
Luke Bayes <lbayes@google.com>
2011-06-01 17:09:49 -0700

    Added reference from observation to Station

--- api/models/station.py
+++ api/models/station.py
@@ -1,9 +1,9 @@
 from google.appengine.ext import db
 from geo.geomodel import GeoModel
-#from api.models.observation import Observation
+from api.models.observation import Observation
 
 class Station(GeoModel):
   station_id = db.StringProperty()
   created_at = db.DateTimeProperty(auto_now_add = True)
   updated_at = db.DateTimeProperty(auto_now = True)
-  #most_recent_observation = db.ReferenceProperty(Observation)
+  most_recent_observation = db.ReferenceProperty(Observation)
