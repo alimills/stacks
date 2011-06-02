723e7d64000a621d87362163f45016defe466097
Luke Bayes <lbayes@google.com>
2011-06-01 17:09:41 -0700

    Added reference from observation to Station

--- api/models/observation.py
+++ api/models/observation.py
@@ -1,7 +1,8 @@
 from google.appengine.ext import db
 from geo.geomodel import GeoModel
+from api.models.station import Station
 
 class Observation(GeoModel):
-  station_id = db.StringProperty()
+  station = db.ReferenceProperty(Station)
   created_at = db.DateTimeProperty(auto_now_add = True)
   observed_at = db.DateTimeProperty()
--- test/test_observation.py
+++ test/test_observation.py
@@ -1,6 +1,7 @@
 import datetime
 from test.stack_test_case import StackTestCase
 from api.models.observation import Observation
+from api.models.station import Station
 
 class TestObservation(StackTestCase):
 
@@ -9,5 +10,7 @@ class TestObservation(StackTestCase):
     self.assertTrue(instance)
 
   def testCreateWithParam(self):
-    instance = Observation(station_id = "abcd")
-    self.assertEqual("abcd", instance.station_id)
+    station = Station(station_id = "abcd1234")
+    station.put()
+    instance = Observation(station = station)
+    self.assertEqual("abcd1234", instance.station.station_id)
